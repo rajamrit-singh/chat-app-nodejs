07999b287f9538c3ecb7c955abfbc8455d306440
rajamrit-singh <rajamritsingh1996@gmail.com>
2023-06-23 23:31:32 -0700

    added timestamps

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,19 +9,34 @@ const $messages = document.querySelector('#messages');
 
 //templates
 const $messageTemplate = document.querySelector('#message-template').innerHTML;
+const $locationTemplate = document.querySelector('#location-template').innerHTML;
 
 socket.on('message' , (message) => {
     console.log(message);
     const html = Mustache.render($messageTemplate, {
-        message
+        message: message.text,
+        createdAt: moment(message.createdAt).format('hh:mm A')
     });
     $messages.insertAdjacentHTML('beforeend', html)
 });
+
+socket.on('locationMessage', (message) => {
+    console.log(message);
+    const html = Mustache.render($locationTemplate, {
+        url: message.url,
+        createdAt: moment(message.createdAt).format('hh:mm A')
+    });
+    $messages.insertAdjacentHTML('beforeend', html)
+})
 
 $messageForm.addEventListener('submit', (event) => {
     event.preventDefault();
     $messageFormButton.setAttribute('disabled', 'disabled');
     const inputMessage = event.target.elements.message.value;   //We can acced the name (message) on elements
+    if (!inputMessage.trim()) {
+        $messageFormButton.removeAttribute('disabled');
+        return;
+    }
     socket.emit('sendMessage', inputMessage, (error) => {
         $messageFormButton.removeAttribute('disabled');
         $messageFormInput.value = '';
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io')
 var Filter = require('bad-words');
+const { generateMessage, generateLocationMessage } = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -19,23 +20,23 @@ app.use(express.static(publicDirectoryPath));
 
 io.on('connection', (socket) => {
     console.log('new web socket')
-    socket.emit('message', 'Welcome');
-    socket.broadcast.emit('message', 'A new user has joined')   //this will broadcast the message to everyone except the current user
+    socket.emit('message', generateMessage('Welcome'));
+    socket.broadcast.emit('message', generateMessage('A new user has joined'));   //this will broadcast the message to everyone except the current user
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter();
         if (filter.isProfane(message)) {
             return callback('Profanity is not allowed');
         }
-        io.emit('message', message);
+        io.emit('message', generateMessage(message));
         callback('delivered' );
     })
 
     socket.on('disconnect', () => {     //disconnect is a built-in event when user disconnected
-        io.emit('message', 'A user has left');
+        io.emit('message', generateMessage('A user has left' ));
     })
 
     socket.on('sendLocation', ({ latitude, longitude }, callback) => {
-        io.emit('message', `https://google.com/maps?q=${latitude},${longitude}`)
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${latitude},${longitude}`));
         callback();
     })
 })
